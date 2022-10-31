5a32ff6fb73600613968aabe0698f425db4f998c
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2022-10-31 12:37:53 +0530

    puzzles

--- general-puzzles/src/main/kotlin/FindElementsWhoseSumIsN.kt
+++ general-puzzles/src/main/kotlin/FindElementsWhoseSumIsN.kt
@@ -1,20 +1,31 @@
+import kotlin.math.abs
+
 object FindElementsWhoseSumIsN {
     @JvmStatic
     fun main(args: Array<String>) {
-        val n = 23
+        val n = 21
         val arr = arrayOf(2, 4, 7, 19, 21)
-        println(findElements(arr, n))
+        val tolerance = 1.0
+
+        println(findElements(arr, n, tolerance))
     }
 
-    private fun findElements(arr: Array<Int>, n: Int): List<Int> {
+    private fun findElements(arr: Array<Int>, n: Int, tolerance: Double = 0.0): List<Int> {
         val gathered = mutableListOf<Int>()
-        parts(n, arr, gathered, 0)
+        parts(n, arr, gathered, 0, n, tolerance)
         return gathered
     }
 
-    private fun parts(n: Int, arr: Array<Int>, gathered: MutableList<Int>, acc: Int, target: Int = n) {
+    private fun parts(
+        n: Int,
+        arr: Array<Int>,
+        gathered: MutableList<Int>,
+        acc: Int,
+        target: Int = n,
+        tolerance: Double
+    ) {
         if (n <= 0) {
-            if (acc != target) {
+            if (acc != target && abs(acc - target) > tolerance) {
                 gathered.clear()
             }
             return
@@ -27,6 +38,6 @@ object FindElementsWhoseSumIsN {
         }
 
         gathered.add(closest)
-        parts(n - closest, arr, gathered, acc + closest, target)
+        parts(n - closest, arr, gathered, acc + closest, target, tolerance)
     }
 }
